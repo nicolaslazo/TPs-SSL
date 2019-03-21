da9ad1985af9b4a7e8f289fde4ea20dbfc24d4f3
oh-well <nlazo@est.frba.utn.edu.ar>
2019-03-20 21:05:07 -0300

    Uh mezclé todo, también creé el output.txt y puse un recordatorio para el pobre que tenga que rellenar el modelo de testeo

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-char determinarCategoria(char charLeido) {
+int determinarCategoria(char charLeido) {
 	// Determina la categoria de cambio de estado.
 	
 	/* Tabla de outputs:
-	 * 	'0': {0}
-	 * 	'7': [0-7]
-	 * 	'9': [8-9]
-	 * 	'x': {x, X}
-	 * 	'z': [A-Z] U [a-z]
+	 * 	0: {0}
+	 * 	1: [1-7]
+	 * 	2: [8-9]
+	 * 	3: [A-Z] U [a-z]
+	 * 	4: {x, X}
+	 * 	5: Otros
 	 */
 
-	// Les diria que dejemos esa tabla en el comentario como documentacion del programa, es mas facil para mostrarle al profe
+	// Les dije que quedaria mejor como un char, pero me di cuenta de que era agregar pasos al pedo. Ahora es un index en la tabla de transiciones.
+	// Les diria que dejemos esa tabla en el comentario como documentacion del programa.
 
 	/* Tip: se puede usar
 	 * 	if ( charLeido >= 'a' && charLeido <= 'z' )
@@ -27,19 +29,38 @@ void escribirResultado(int estado) {
 	char stringAEscribir[40];
 
 	switch (estado) {
-		
+		// TODO: Asignarle a stringAEscribir el renglon que se tiene que escribir en el archivo
 	}
 
-	// TODO: Completar
+	// TODO: Hacer la parte de guardar en archivo
 }
 
 int main() {
-	unsigned int estado = 0;
+	int estado = 0;
 
-	// TODO: Hacer lectura de archivo
-	// TODO: Hacer la tabla de transicion
-	// TODO: Crear el main loop
+	char charLeido;
 
+	int transiciones[6][7] = { 
+		                   {2, 1, 1, 6, 6, 6},
+				   {1, 1, 1, 6, 6, 6},
+				   {4, 4, 6, 6, 3, 6},
+				   {5, 5, 5, 5, 6, 6},
+				   {4, 4, 6, 6, 6, 6},
+				   {5, 5, 5, 5, 6, 6},
+				   {6, 6, 6, 6, 6, 6}
+				 };
+
+	while ( /* No se llega a EOF */ ) {
+		// TODO: Hacer la lectura de char
+
+		if ( charLeido == ',' ) {
+			escribirResultado(estado);
+			estado = 0;
+		}
+		else {
+			estado = transiciones[determinarCategoria(charLeido)][estado];
+		}
+	}
 
 	return 0;
 }
