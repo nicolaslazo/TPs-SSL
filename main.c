eb017627d2c97e129b52dbd8ccd46c17b7f9c039
EzequielUS <36769195+EzequielUS@users.noreply.github.com>
2019-03-26 13:53:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int determinarCategoria(char charLeido) {
-	// Determina la categoria de cambio de estado.
-	
+int determinarCategoria(char charLeido) // Determina la categoria de cambio de estado.
+{
 	/* Tabla de outputs:
 	 * 	0: {0}
 	 * 	1: [1-7]
 	 * 	2: [8-9]
-	 * 	3: [A-Z] U [a-z]
+	 * 	3: [A-F] U [a-f]
 	 * 	4: {x, X}
 	 * 	5: Otros
 	 */
@@ -16,30 +15,28 @@ int determinarCategoria(char charLeido) {
 	// Les diria que dejemos esa tabla en el comentario como documentacion del programa.
 
 	/* Tip: se puede usar
-	 * 	if ( charLeido >= 'a' && charLeido <= 'z' )
+	 * 	if ( charLeido >= 'a' && charLeido <= 'f' )
 	 * Para verificar que esta en ese rango
 	 */
 
 	// TODO: Completar
 }
 
-void escribirResultado(int estado) {
-	// Llamada cuando la palabra termina. Escribe a output la palabra y su categoria
-	
+void escribirResultado(int estado) //Funcion que va a devolver un archivo de texto, con la clasificacion de cada cadena de caracteres(separados por una coma)
+{
 	char stringAEscribir[40];
 
-	switch (estado) {
-		// TODO: Asignarle a stringAEscribir el renglon que se tiene que escribir en el archivo
+	switch (estado) // TODO: Asignarle a stringAEscribir el renglon que se tiene que escribir en el archivo
+	{
+		
 	}
 
 	// TODO: Hacer la parte de guardar en archivo
 }
 
-int main() {
+int main() 
+{
 	int estado = 0;
-
-	char charLeido;
-
 	int transiciones[6][7] = { 
 		                   {2, 1, 1, 6, 6, 6},
 				   {1, 1, 1, 6, 6, 6},
@@ -49,18 +46,28 @@ int main() {
 				   {5, 5, 5, 5, 6, 6},
 				   {6, 6, 6, 6, 6, 6}
 				 };
+	FILE* f = fopen( "ArchivoDeTexto.txt" , "r+b" ) //Abro y leo el archivo de texto
 
-	while ( /* No se llega a EOF */ ) {
-		// TODO: Hacer la lectura de char
+	char charLeido; //Guardo lo que leo del archivo de texto 
 
-		if ( charLeido == ',' ) {
+	fread ( &charLeido , sizeof(char) , 1 , f ) //Leo el primer caracter	
+
+	while ( !feof(f) ) 
+	{
+		cout << charLeido << endl; //Muestro el caracter que lei 	
+		
+
+			if ( charLeido == ',' ) 
+			{
 			escribirResultado(estado);
 			estado = 0;
-		}
-		else {
-			estado = transiciones[determinarCategoria(charLeido)][estado];
-		}
+			}
+				else 
+				{
+				estado = transiciones[determinarCategoria(charLeido)][estado]; //Fila-Columna [estado][determinarCategoria(charLeido)] Corregir
+				}
+		fread ( &charLeido , sizeof(char) , 1 , f ) //Leo el siguiente caracter
 	}
-
+	fclose(f);
 	return 0;
 }
