341d2bdc3c437d8238141d0ae5191fe80830f809
oh-well <nlazo@est.frba.utn.edu.ar>
2019-04-19 15:15:40 -0300

    Descripcion de tabla de transiciones

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ *	Formato de tabla de transicion
+ *
+ *	Columnas			Filas
+ *	1	{1, 2, 3, 4, 5}		1	E1
+ *	2	+			2	E2
+ *	3	*			3	T1
+ *	4	(			4	T2
+ *	5	)			5	F1
+ *	6	NULL			6	N
+ *					7	X (RECHAZADO)
+ */
+int tablaDeTransicion[6][7] = {}	// TODO: Averiguar como armar una tabla de transicion
+					// 	 que transiciona a dos items
+
+int main() {
+
+	return 0;
+}
