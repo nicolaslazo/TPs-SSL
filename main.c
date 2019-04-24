879ada67ac981bb658972d2d0742a7cf545e5bef
oh-well <nlazo@est.frba.utn.edu.ar>
2019-04-24 15:30:23 -0300

    Planteo de tabla de transicion y reglas gramaticales

--- main.c
+++ main.c
@@ -2,14 +2,25 @@
 
 /*	Tabla de transicion:
  *			|	Num	+	*	(	)	Lambda
- *		----------------------------------------------------------------
+ *		---------------------------------------------------------------
  *		E	|	TF	-	-	TF	-	-
  *		F	|	-	TF	-	-	-	e
  *		T	|	FU	-	-	FU	-	-
  *		U	|	-	-	FU	-	-	e
- *		F	|	N	-	-	E)	-	-
+ * 	 	P	|	N	-	-	E)	-	-
  *		N	|	e	-	-	-	-	-
  *		X	|	?	?	?	?	?	?
+ *
+ *	Gramatica:
+ *		1. E -> TF
+ *		2. F -> +TF
+ *		3. F -> Lambda
+ *		4. T -> FU
+ *		5. U -> *FU
+ *		6. U -> Lambda
+ *		7. F -> (E)
+ *		8. F -> N
+ *		9. N -> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }
  */
 
 struct Nodo {
@@ -17,8 +28,30 @@ struct Nodo {
 	Nodo *sig = NULL;
 }
 
-enum simbolos {
-	// Herramienta a tener en cuenta, estaria excelente si leen del tema
+enum estados {
+	E,
+	F,
+	T,
+	U,
+	P,
+	N
+}
+
+enum caracteres {
+	NUMERO,
+	MAS,
+	POR,
+	PARENTESIS1,
+	PARENTESIS2,
+	LAMBDA
+}
+
+int tablaDeTransicion[6][6] = { { 1, 0, 0, 1, 0, 0},
+			      { 0, 2, 0, 0, 0, 3},
+			      { 4, 0, 0, 4, 0, 0},
+			      { 0, 0, 5, 0, 0, 6},
+			      { 7, 0, 0, 8, 0, 0},
+			      { 9, 0, 0, 0, 0, 0}
 }
 
 char leerCaracter(); // Esto no tiene que ser una funcion necesariamente, es una abstraccion
