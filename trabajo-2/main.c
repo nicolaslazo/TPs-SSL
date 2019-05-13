9680666151d3198cec9a0ee0aea776cdfdb8d2ab
Alfrel <48848948+Alfrel@users.noreply.github.com>
2019-05-13 13:55:10 -0300

    Update main.c

--- trabajo-2/main.c
+++ trabajo-2/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #define Simbolo Int
+#define vuelveABUSCAR -1
+
+
 //	TODO: Averiguar si hacer este tipo de alias es buena practica
 
 /*	Tabla de transicion:
+
  *			|	Num	+	*	(	)	Lambda
  *		---------------------------------------------------------------
  *		E	|	T1E2	-	-	T1E2	-	-
@@ -45,31 +49,17 @@ void push(Categoria categoria, NodoPila*& pila) {
 }
 
 Simbolo pop(NodoPila*& pila) {
+     //
 }
 
-Simbolo peek(/* ??? */) {
+//Simbolo peek(/* ??? */) {
 	// TODO: Esta funcion deberia leer el caracter de la string input
 	// que estamos procesando y guardarlo en la variable char caracter
-	
-	return determinarSimbolo(caracter)
+
+	//return determinarSimbolo(caracter)
 }
 
-enum simbolos {
-	// No terminales
-	E1,
-	E2,
-	T1,
-	T2,
-	F1,
-	N,
-
-	// Terminales
-	NUMERO,
-	MAS,
-	POR,
-	PARENTESIS1,
-	PARENTESIS2
-}
+enum simbolo {E1,E2,T1,T2,F1,N,NUMERO,MAS,POR,PARENTESIS1,PARENTESIS2}
 
 Simbolo determinarSimbolo(char lectura) {
 	switch ( lectura ) {
@@ -93,35 +83,126 @@ Simbolo determinarSimbolo(char lectura) {
 		case ')':
 			return PARENTESIS2;
 		default:
-			reportarError("Input invalido");
+		    {if (lectura==' ' || lectura == '\n' || lectura == '\t')
+		         return vuelveABUSCAR;
+			else reportarError("Input invalido");
+		    }
 	}
 }
 
-void ejecutarTransicion(/* String de input? */, NodoPila*& pila) {
-	Simbolo simboloActual = pop(pila);
-	Simbolo sigSimbolo = peek(/* input? */);
+void ejecutarTransicion(/* caracter de String input? */, NodoPila*& pila) {
+    enum simbolo simboloActual;
+	bool buscaSiguiente; //es true al principio o cuando se hace un pop de la pila (más adelante)
 
-	switch ( simboloActual ) {
-		case E1:
+	if (pila.simbolo = Null)
+     {
+       buscaSiguiente=true;
+	   push(E1, pila); // carga el axioma
+     }
+
+	simboloActual = pop(pila);
+
+	if (buscaSiguiente)
+    //Simbolo sigSimbolo = peek(/* caracter de String input */);
+     Simbolo sigSimbolo = determinarSimbolo(charInput[&i])
+
+     if (sigSimbolo != vuelveABUSCAR){ // pregunta si es espacio en blanco, si no vuelve a buscar
+
+	switch (simboloActual) {
+		case E1 // así o el numero asociado enum?
+        {
 			if (sigSimbolo == NUMERO || sigSimbolo == PARENTESIS1) {
+                push(E2, pila);
 				push(T1, pila);
-				push(E2, pila);
+				buscaSiguiente=false;
 			}
 			else {
-				/* ??? */
+				/* reportarError("Input inválido")?*/
+			}
+        }
+			break;
+        case T1
+        {
+			if (sigSimbolo == NUMERO || sigSimbolo == PARENTESIS1) {
+                push(T2, pila);
+				push(F1, pila);
+				buscaSiguiente=false;
+			}
+			else {
+				/* reportarError("Input inválido")? */
+			}
+        }
+			break;
+
+        case F1
+        {
+			if (sigSimbolo == NUMERO)
+                push(N, pila);
+            if (sigSimbolo == PARENTESIS1)
+                {push (PARENTESIS2, pila);
+                push (E1, pila);
+				buscaSiguiente=false;
+                }
+        }
+			else {
+				/* reportarError("Input inválido")? */
 			}
 			break;
-		default:
-			/* ??? */
-	}
+
+	     case N
+        {
+			if (sigSimbolo == NUMERO)
+                pop(pila);
+                &i++;
+
+			else {
+				/* reportarError("Input inválido")? */
+			}
+        }
+			break;
+			case T2
+        {
+			if (sigSimbolo == MAS){
+                push (T2, pila);
+                push (F1, pila);
+			     }
+			else {
+				pop (pila);
+				&i++;
+			     }
+        }
+        break;
+        case E2
+        {
+			if (sigSimbolo == POR){
+                push (E2, pila);
+                push (T1, pila);
+			     }
+			else {
+				pop (pila);
+				&i++;
+			     }
+        }
+        break;
+        default: /*reportarError("Input inválido")*/
+
+	   }
+     }
+     else {
+        buscaSiguiente = true;
+        &i++;
+     }
 }
 
 ////	ACA EMPIEZA LA SECCION DE EZE, SOL Y VICTORIA
 
 int main() {
+
+char charInput[longitudInput];
+
 	do {
-		ejecutarTransicion(/* ??? */);
-	} while (/* No se llega a EOF*/);
+		ejecutarTransicion(/* charInput[&i] ? */);
+	} while (/* &i<longitudInput*/);
 
 	return 0;
 }
