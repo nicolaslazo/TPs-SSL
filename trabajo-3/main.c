196037277fa6e9a115fbc2f949615bd933f6ca5a
oh-well <nlazo@est.frba.utn.edu.ar>
2019-05-29 13:04:35 -0300

    Comentado del comentario al principio de main.c

--- trabajo-3/main.c
+++ trabajo-3/main.c
@@ -1,22 +1,24 @@
-Categorias Lexicas                                                                             ERX
+/*
+	Categorias Lexicas			ERX
 
-constantes enteras decimal                                                               [1-9][0-9]*
-constantes enteras octal                                                                 0[0-7]*
-constantes enteras hexadecimal                                                           0[xX][0-9a-fA-F]+
-constantes reales                                                                        [0-9]*\.[0-9]+([eE][\+\-]?[0-9]+)? |
-                                                                                         [0-9]+\.([eE][\+\-]?[0-9]+)?       |
-                                                                                         [0-9]+([eE][\+\-]?[0-9]+)?
-constante caracter                                                                       '[ -&\(-~]' | \\[t n 0 ' ...]
-literal cadena                                                                           \"([ -!#-~] | \\\")*\"
-palabras reservadas                                                                      auto | break | case | char | const | continue | default
-                                                                                         | do | double | else | enum | extern | float | for | goto
-                                                                                         | if | int | long | register | return | short | signed |
-                                                                                         sizeof | static | struct | switch | typedef | union |
-                                                                                         unsigned | void | volatile | while 
-identificadores                                                                          [a-z A-Z _][a-z A-Z 0-9 _]*
-caracteres de puntuacion                                                                 [, . ; : ... ¿? ¡! () [] {} " ' « »_ - ¨ / * §]
-operadores de c                                                                          [=!&*+-/|%><?]
-comentarios                                                                              \/\/(.)*
+	constantes enteras decimal		[1-9][0-9]*
+	constantes enteras octal		0[0-7]*
+	constantes enteras hexadecimal		0[xX][0-9a-fA-F]+
+	constantes reales			[0-9]*\.[0-9]+([eE][\+\-]?[0-9]+)? 
+		| 				[0-9]+\.([eE][\+\-]?[0-9]+)?
+		|				[0-9]+([eE][\+\-]?[0-9]+)?
+	constante caracter			'[ -&\(-~]' | \\[t n 0 ' ...]
+	literal cadena				\"([ -!#-~] | \\\")*\"
+	palabras reservadas			auto | break | case | char | const | continue | default
+		| 				do | double | else | enum | extern | float | for | goto
+		| 				if | int | long | register | return | short | signed 
+		|				sizeof | static | struct | switch | typedef | union 
+		|				unsigned | void | volatile | while 
+	identificadores				[a-z A-Z _][a-z A-Z 0-9 _]*
+	caracteres de puntuacion		[, . ; : ... ¿? ¡! () [] {} " ' « »_ - ¨ / * §]
+	operadores de c				[=!&*+-/|%><?]
+	comentarios				\/\/(.)*
+*/
 
 /*Listados*/
 
