5ffcd1bad956bf05d404689015d758edd82f8009
oh-well <nlazo@est.frba.utn.edu.ar>
2019-05-29 12:52:13 -0300

    Ordenado de archivos, creado de LibLista.h

---
+++ trabajo-3/LibLista.h
@@ -0,0 +1,13 @@
+/*
+ * 	LibLista.h: Libreria de utilidades basicas para uso de listas
+ */
+
+typedef struct nodo {
+	int val;
+	struct nodo * sig;
+} NodoLista;
+
+
+void agregarALista(int valor, NodoLista * lista) {
+
+}
