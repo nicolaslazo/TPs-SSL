4a0eac89e61c6c36ba5c6ddadebbc24133ce5459
Victoria Guillosson <48684404+victoriaguillosson@users.noreply.github.com>
2019-09-09 23:59:30 -0300

    Update reportes.c

--- trabajo-3/reportes.c
+++ trabajo-3/reportes.c
@@ -1,11 +1,32 @@
-{constDecimales}			agregarAListaInts(atoi(yytext), listaDecimales);
-{constOctales}				agregarAListaInts(atoi(yytext), listaOctales);
-{constHexadecimales}			agregarAListaInts(atof(yytext), listaHexadecimales);
-{constReales}				agregarAListaFloats(atof(yytext), listaReales);
-{literalCadena}				agregarAListaSimple(yytext, listaLiteralesCadena);
-{palabrasReservadas}			agregarAListaSimple(yytext, listaPalabrasReservadas);
-{identificadores}			agregarAListaConCant(yytext, listaIdentificadores);
-{caracteresDePuntuacion}		agregarAListaConCant(yytext, listaCaracteresDePuntuacion);
-{operadoresDeC}				agregarAListaConCant(yytext, listaOperadores);
-{comentarios}				agregarAListaSimple(yytext, listaComentarios);
+{constDecimales}			        {acum= acum + atoi(yytext); printf("El valor acumulado es %d\n", acum);}
+{constOctales}				        {printf("La cadena %s representa una constante entera octal\n", yytext);}
+{constHexadecimales}			    {printf("La cadena %s representa una constante entera hexadecimal\n", yytext);}
+{constReales}				          {printf("La cadena %s representa una constante real\n", yytext);}
+{literalCadena}				
+{palabrasReservadas}			    {printf("La cadena %s representa una palabra reservada\n", yytext);}
+{identificadores}			        {printf("La cadena %s representa un identificador\n", yytext);}
+{caracteresDePuntuacion}		
+{operadoresDeC}				
+{comentarios}				
 .	
+int listadoDeIdentificadores //cant de veces q aparecen listaId
+
+int listadoDeLiteralesCadena //longitud d los mismos listaLiteralesCadena
+
+struct listadoDePalabrasReservadas // listaPalabrasReservadAS
+
+int listadoDeConstantesOctales //valor entero decimal listaOctales
+
+int listadoDeConstantesHexadecimales //valor entero decimal listaHexadecimales
+
+float listadoDeConstantesDecimales //valor acumulado listaDecimales
+
+float listadoDeConstantesReales //valor de mantisa y parte entera listaReales
+
+int listadoDeCaracteresDePuntuacion //cant de veces q aparecen listaCaracteresDePuntuacion
+
+int listadoDeOperadores //cant de veces q aparecen listaOperadores
+
+listadoDeComentarios //listaComentarios
+
+listadoDeCadenasNoReconocidas
