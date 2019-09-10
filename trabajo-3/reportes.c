b347b0c4171c7672c3bb9e4a0bd5fa29a9b021df
Victoria Guillosson <48684404+victoriaguillosson@users.noreply.github.com>
2019-09-10 00:24:29 -0300

    Faltan terminar y darles forma a los reportes

--- trabajo-3/reportes.c
+++ trabajo-3/reportes.c
@@ -1,8 +1,10 @@
+int acum = 0;
+%%
 {constDecimales}			        {acum= acum + atoi(yytext); printf("El valor acumulado es %d\n", acum);}
 {constOctales}				        {printf("La cadena %s representa una constante entera octal\n", yytext);}
 {constHexadecimales}			    {printf("La cadena %s representa una constante entera hexadecimal\n", yytext);}
 {constReales}				          {printf("La cadena %s representa una constante real\n", yytext);
-                                 int dec = octToDec(yytext);
+                              int dec = octToDec(yytext);
 {literalCadena}				        {printf("La cadena %s representa un literal cadena\n", yytext);}
 {palabrasReservadas}			    {printf("La cadena %s representa una palabra reservada\n", yytext);}
 {identificadores}			        {printf("La cadena %s representa un identificador\n", yytext);}
@@ -10,7 +12,7 @@
 {operadoresDeC}				
 {comentarios}				          {printf("La cadena %s representa un comentario\n", yytext);} 
 
-                               
+%%                               
 int octToDec(char oct[]){
     int i=0,dec=0;
     for(i,oct[i]!='\0',i++);
